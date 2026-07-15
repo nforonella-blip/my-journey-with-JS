8d48fd8c52d3846a60664f6e5a022b03fe1ba90c
nforonella-blip <nforonella@gmail.com>
2026-07-15 23:21:44 +0100

    Add files via upload

---
+++ toturals/script.js
@@ -0,0 +1,16 @@
+// decarindg the variables5
+const myName ="ornella";
+myfirstName="Onella"
+const myAge = 18;
+const department = "software engineer";
+const isprofessional= true
+console.log("My name is "+ myName + ", I am " + myAge + " i work as" + department + ". Am i consistent ");
+
+    // decaring the variable
+
+    const Number1 =20;
+    const Number2 = 30;
+     console.log("Number1 + Number2 = " + (Number1 + Number2));
+     console.log("Number1 - Number2 = " + (Number1 - Number2));
+     console.log("Number1 / Number2 = " + (Number1 / Number2));
+     console.log("Number1 * Number2 = " + (Number1 * Number2));
